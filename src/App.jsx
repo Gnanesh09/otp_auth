3d3d40db95e62cf81b2058ed4e8bc14648714ab5
Gnanesh09 <gnanesh9206@gmail.com>
2025-07-30 12:47:58 +0530

    adding frontend on git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react'
+import OTPVerification from './components/OTPVerification'
+
+function App() {
+  return (
+    <div className="App">
+      <OTPVerification />
+    </div>
+  )
+}
+
+export default App
