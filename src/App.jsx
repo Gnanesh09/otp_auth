fa34fe56aaf5ae71cf35b9473f010600abdbb203
Gnanesh09 <gnanesh9206@gmail.com>
2025-07-30 13:30:48 +0530

    adding web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // src/App.jsx
 import React from 'react'
 import OTPVerification from './components/OTPVerification'
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <OTPVerification />
     </div>
   )
